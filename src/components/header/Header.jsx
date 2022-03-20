bffea9157f84f571fa2f013be5e028017cffe58e
jhhg0 <jhherrera@ccss.sa.cr>
2022-03-20 08:27:10 -0500

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm </h5>
+        <h1>John </h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
